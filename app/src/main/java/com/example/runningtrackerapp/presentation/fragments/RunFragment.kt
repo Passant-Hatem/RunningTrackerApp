b9085851e91f9c165dd305b0a8e2dc39a9e7bd0f
Passant-Hatem <79157384+Passant-Hatem@users.noreply.github.com>
2022-10-30 01:43:21 +0200

    feat: setup location permissions

--- app/src/main/java/com/example/runningtrackerapp/presentation/fragments/RunFragment.kt
+++ app/src/main/java/com/example/runningtrackerapp/presentation/fragments/RunFragment.kt
@@ -1,13 +1,105 @@
 package com.example.runningtrackerapp.presentation.fragments
 
+import android.Manifest
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.runningtrackerapp.R
+import com.example.runningtrackerapp.databinding.FragmentRunBinding
 import com.example.runningtrackerapp.presentation.viewmodels.MainViewModel
+import com.example.runningtrackerapp.util.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.vmadalin.easypermissions.dialogs.SettingsDialog
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class RunFragment : Fragment(R.layout.fragment_run) {
-
+class RunFragment : Fragment(), EasyPermissions.PermissionCallbacks {
     private val viewModel: MainViewModel by viewModels()
+
+    private var _binding: FragmentRunBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRunBinding.inflate(inflater, container, false)
+        requestLocationPermissions()
+        return binding.root
+    }
+
+    private fun requestLocationPermissions() {
+        if(hasLocationPermission()){
+            return
+        }else{
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app.",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    private fun hasLocationPermission() =
+        EasyPermissions.hasPermissions(
+            requireContext(),
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+    private fun requestBackgroundLocationPermissions() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q){
+            if(hasBackgroundLocationPermission()){
+                return
+            }else{
+                EasyPermissions.requestPermissions(
+                    this,
+                    "You need to allow background location permissions to use this app.",
+                    REQUEST_CODE_LOCATION_PERMISSION,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                )
+            }
+        }
+    }
+
+   @RequiresApi(Build.VERSION_CODES.Q)
+   private fun hasBackgroundLocationPermission() =
+        EasyPermissions.hasPermissions(
+            requireContext(),
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        )
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            SettingsDialog.Builder(requireContext()).build().show()
+        }else{
+            requestLocationPermissions()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        requestBackgroundLocationPermissions()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/example/runningtrackerapp/presentation/fragments/SetupFragment.kt
+++ app/src/main/java/com/example/runningtrackerapp/presentation/fragments/SetupFragment.kt
@@ -1,9 +1,42 @@
 package com.example.runningtrackerapp.presentation.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.runningtrackerapp.R
+import com.example.runningtrackerapp.databinding.FragmentSetupBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SetupFragment : Fragment(R.layout.fragment_setup) {
+class SetupFragment : Fragment() {
+    private var _binding: FragmentSetupBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSetupBinding.inflate(inflater, container, false)
+
+        binding.tvContinue.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_setupFragment_to_runFragment
+            )
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
