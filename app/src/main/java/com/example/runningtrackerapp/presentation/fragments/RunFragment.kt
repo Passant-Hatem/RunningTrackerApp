2092ff00b5c389b320e84d9e3db3a44753fae06a
Passant-Hatem <79157384+Passant-Hatem@users.noreply.github.com>
2022-11-12 00:37:31 +0200

    feat: sort runs

--- app/src/main/java/com/example/runningtrackerapp/presentation/fragments/RunFragment.kt
+++ app/src/main/java/com/example/runningtrackerapp/presentation/fragments/RunFragment.kt
@@ -6,10 +6,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.runningtrackerapp.R
@@ -17,6 +17,7 @@ import com.example.runningtrackerapp.databinding.FragmentRunBinding
 import com.example.runningtrackerapp.presentation.adapter.RunAdapter
 import com.example.runningtrackerapp.presentation.viewmodels.MainViewModel
 import com.example.runningtrackerapp.util.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.example.runningtrackerapp.util.SortType
 import com.vmadalin.easypermissions.dialogs.SettingsDialog
 import dagger.hilt.android.AndroidEntryPoint
 import pub.devrel.easypermissions.EasyPermissions
@@ -54,7 +55,29 @@ class RunFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
         setupRecyclerView()
 
-        viewModel.runsSortedByDate.observe(viewLifecycleOwner) {
+        when(viewModel.sortType) {
+            SortType.DATE -> binding.spFilter.setSelection(0)
+            SortType.RUNNING_TIME ->  binding.spFilter.setSelection(1)
+            SortType.DISTANCE ->  binding.spFilter.setSelection(2)
+            SortType.AVG_SPEED ->  binding.spFilter.setSelection(3)
+            SortType.CALORIES_BURNED ->  binding.spFilter.setSelection(4)
+        }
+
+        binding.spFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+                when(pos) {
+                    0 -> viewModel.sortRuns(SortType.DATE)
+                    1 -> viewModel.sortRuns(SortType.RUNNING_TIME)
+                    2 -> viewModel.sortRuns(SortType.DISTANCE)
+                    3 -> viewModel.sortRuns(SortType.AVG_SPEED)
+                    4 -> viewModel.sortRuns(SortType.CALORIES_BURNED)
+                }
+            }
+        }
+
+        viewModel.runs.observe(viewLifecycleOwner) {
             runAdapter.submitList(it)
         }
 
@@ -65,7 +88,6 @@ class RunFragment : Fragment(), EasyPermissions.PermissionCallbacks {
         adapter = runAdapter
         layoutManager = LinearLayoutManager(requireContext())
     }
-
 
     private fun requestLocationPermissions() {
         if(hasLocationPermission()){
