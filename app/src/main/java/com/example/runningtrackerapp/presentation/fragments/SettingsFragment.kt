57f215cd841b2550180d57f9af27f6e96cefc243
Passant-Hatem <79157384+Passant-Hatem@users.noreply.github.com>
2022-11-13 00:21:35 +0200

    feat: static screen

--- app/src/main/java/com/example/runningtrackerapp/presentation/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/runningtrackerapp/presentation/fragments/SettingsFragment.kt
@@ -1,10 +1,73 @@
 package com.example.runningtrackerapp.presentation.fragments
 
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-
+import androidx.navigation.fragment.findNavController
 import com.example.runningtrackerapp.R
+import com.example.runningtrackerapp.databinding.FragmentRunBinding
+import com.example.runningtrackerapp.databinding.FragmentSettingsBinding
+import com.example.runningtrackerapp.util.Constants.KEY_NAME
+import com.example.runningtrackerapp.util.Constants.KEY_WEIGHT
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class SettingsFragment : Fragment(R.layout.fragment_settings) {
+class SettingsFragment : Fragment() {
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadFieldsFromSharedPref()
+        binding.btnApplyChanges.setOnClickListener {
+            val success = applyChangesToSharedPref()
+            if(success) {
+                Snackbar.make(view, "Saved changes", Snackbar.LENGTH_LONG).show()
+            } else {
+                Snackbar.make(view, "Please fill out all the fields", Snackbar.LENGTH_LONG).show()
+            }
+        }
+
+
+    }
+
+    private fun loadFieldsFromSharedPref() {
+        val name = sharedPreferences.getString(KEY_NAME, "")
+        val weight = sharedPreferences.getFloat(KEY_WEIGHT, 80f)
+        binding.etName.setText(name)
+        binding.etWeight.setText(weight.toString())
+    }
+
+    private fun applyChangesToSharedPref(): Boolean {
+        val nameText =  binding.etName.text.toString()
+        val weightText =  binding.etWeight.text.toString()
+        if(nameText.isEmpty() || weightText.isEmpty()) {
+            return false
+        }
+        sharedPreferences.edit()
+            .putString(KEY_NAME, nameText)
+            .putFloat(KEY_WEIGHT, weightText.toFloat())
+            .apply()
+        val toolbarText = "Let's go $nameText"
+        requireActivity().actionBar?.title = toolbarText
+        return true
+    }
+
 }
--- app/src/main/java/com/example/runningtrackerapp/presentation/viewmodels/StatisticsViewModel.kt
+++ app/src/main/java/com/example/runningtrackerapp/presentation/viewmodels/StatisticsViewModel.kt
@@ -2,9 +2,22 @@ package com.example.runningtrackerapp.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
 import com.example.runningtrackerapp.domain.repository.MainRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class StatisticsViewModel @Inject constructor(
     val mainRepository: MainRepository
 ): ViewModel() {
+
+    val totalTimeRun = mainRepository.getTotalTimeInMillis()
+    val totalDistance = mainRepository.getTotalDistance()
+    val totalCaloriesBurned = mainRepository.getTotalCaloriesBurned()
+    val totalAvgSpeed = mainRepository.getTotalAvgSpeed()
+
+    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
+
+
+
+
 }
